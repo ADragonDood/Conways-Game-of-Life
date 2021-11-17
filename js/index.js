32cd34b940448c15501b1bf0c38407322f066aee
ADragonDood <53982960+ADragonDood@users.noreply.github.com>
2021-11-17 16:45:24 +0000

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,4 +1,18 @@
 var repeat
+var cachedPositions = []
+
+function getNeighbours(cellPosInRow, cellRow) {
+    let leftNeighbour = document.getElementById("cell-" + (cellPosInRow - 1) + "-row-" + cellRow)
+    let topLeftNeighbour = document.getElementById("cell-" + (cellPosInRow - 1) + "-row-" + (cellRow - 1))
+    let topNeighbour = document.getElementById("cell-" + cellPosInRow + "-row-" + (cellRow - 1))
+    let topRightNeighbour = document.getElementById("cell-" + (cellPosInRow + 1) + "-row-" + (cellRow - 1))
+    let rightNeighbour = document.getElementById("cell-" + (cellPosInRow + 1) + "-row-" + cellRow)
+    let bottomRightNeighbour = document.getElementById("cell-" + (cellPosInRow + 1) + "-row-" + (cellRow + 1))
+    let bottomNeighbour = document.getElementById("cell-" + cellPosInRow + "-row-" + (cellRow + 1))
+    let bottomLeftNeighbour = document.getElementById("cell-" + (cellPosInRow - 1) + "-row-" + (cellRow + 1))
+
+    return [leftNeighbour, topLeftNeighbour, topNeighbour, topRightNeighbour, rightNeighbour, bottomRightNeighbour, bottomNeighbour, bottomLeftNeighbour]
+}
 
 function createGrid() {
     let grid = document.getElementById("grid");
@@ -15,7 +29,7 @@ function createGrid() {
         for (let j = 0; j < (grid.clientWidth/10); j++) {
 
             let cellObj = document.createElement("div");
-            cellObj.className = "cell";
+            cellObj.className = "cell dead";
             cellObj.id = "cell-"+j+"-row-"+i;
             cellObj.setAttribute("value", "dead");
             cellObj.setAttribute("onclick", "setState(this);");
@@ -29,11 +43,32 @@ function setState(cell) {
     let value = cell.getAttribute("value")
     if (value == "dead") {
         cell.setAttribute("value", "alive")
-        cell.style.backgroundColor = "black"
+        cell.className = "cell alive"
     } else {
         cell.setAttribute("value", "dead")
-        cell.style.backgroundColor = "white"
+        cell.className = "cell dead"
     }
+}
+
+function Clear() {
+    let cells = Array.prototype.slice.call(document.getElementsByClassName("cell"))
+
+    cells.forEach(cell => {
+        cell.setAttribute("value", "dead")
+        cell.className = "cell dead"
+    })
+
+    clearInterval(repeat)
+}
+
+function loadCachedPositions(cachedPositions) {
+    Clear()
+    cachedPositions.forEach(cell => {
+        let cellPos = document.getElementById(cell.id)
+
+        cellPos.setAttribute("value", "alive")
+        cellPos.className = "cell alive"
+    })
 }
 
 function Pause() {
@@ -41,6 +76,15 @@ function Pause() {
 }
 
 function Start() {
+    cachedPositions = []
+    let cells = Array.prototype.slice.call(document.getElementsByClassName("cell"))
+
+    cells.forEach(cell => {
+        if (cell.getAttribute("value") == "alive") {
+            cachedPositions.push(cell)
+        }
+    })
+
     repeat = setInterval(checkCell, 100)
 }
 
@@ -49,51 +93,60 @@ function checkCell() {
     let cellsPerRow = document.getElementsByClassName("row")[0].childElementCount
 
     let updateArr = []
+    let cellsToCheck = []
 
     for (let i=0; i<rowCount; i++) {
         for (let j=0; j<cellsPerRow; j++) {
             let cell = document.getElementById("cell-"+j+"-row-"+i)
 
-            let totalAliveNeighbours = 0
+            if (cell.getAttribute("value") == "alive") {
+                cellsToCheck.push(cell)
 
-            let leftNeighbour = document.getElementById("cell-"+(j-1)+"-row-"+i)
-            let topLeftNeighbour = document.getElementById("cell-"+(j-1)+"-row-"+(i-1))
-            let topNeighbour = document.getElementById("cell-"+j+"-row-"+(i-1))
-            let topRightNeighbour = document.getElementById("cell-"+(j+1)+"-row-"+(i-1))
-            let rightNeighbour = document.getElementById("cell-"+(j+1)+"-row-"+i)
-            let bottomRightNeighbour = document.getElementById("cell-"+(j+1)+"-row-"+(i+1))
-            let bottomNeighbour = document.getElementById("cell-"+j+"-row-"+(i+1))
-            let bottomLeftNeighbour = document.getElementById("cell-"+(j-1)+"-row-"+(i+1))
+                let neighbourArr = getNeighbours(j, i)
 
-            let neighbourArr = [leftNeighbour, topLeftNeighbour, topNeighbour, topRightNeighbour, rightNeighbour, bottomRightNeighbour, bottomNeighbour, bottomLeftNeighbour]
-
-            neighbourArr.forEach(neighbour => {
-                if (typeof neighbour == 'undefined' || !neighbour) {
-                    return
-                } else {
-                    if (neighbour.getAttribute("value") == "alive") {
-                        totalAliveNeighbours++
-                    } else {
-                        return
-                    }
-                }
-            })
-
-            if (totalAliveNeighbours < 2) {
-
-                updateArr.push([`${cell.id}`, "dead"])
-
-            } else if (totalAliveNeighbours > 3) {
-
-                updateArr.push([`${cell.id}`, "dead"])
-
-            } else if (totalAliveNeighbours == 3) {
-
-                updateArr.push([`${cell.id}`, "alive"])
-
+                neighbourArr.forEach(neighbour => {
+                    if (!neighbour) return
+                    cellsToCheck.push(neighbour)
+                })
             }
         }
     }
+
+    cellsToCheck.forEach(cell => {
+        let totalAliveNeighbours = 0
+
+        let cellIdSplit = cell.id.toString().split("-")
+        let cellPosInRow = parseInt(cellIdSplit[1])
+        let cellRow = parseInt(cellIdSplit[3])
+        
+        let neighbourArr = getNeighbours(cellPosInRow, cellRow)
+
+        neighbourArr.forEach(neighbour => {
+            if (typeof neighbour == 'undefined' || !neighbour) {
+                return
+            } else {
+                if (neighbour.getAttribute("value") == "alive") {
+                    totalAliveNeighbours++
+                } else {
+                    return
+                }
+            }
+        })
+
+        if (totalAliveNeighbours < 2) {
+
+            updateArr.push([`${cell.id}`, "dead"])
+
+        } else if (totalAliveNeighbours > 3) {
+
+            updateArr.push([`${cell.id}`, "dead"])
+
+        } else if (totalAliveNeighbours == 3) {
+
+            updateArr.push([`${cell.id}`, "alive"])
+
+        }
+    })
 
     updateCells(updateArr)
 }
@@ -104,10 +157,10 @@ function updateCells(updateArr) {
 
         if (cellUpdate[1] == "alive") {
             cell.setAttribute("value", "alive")
-            cell.style.backgroundColor = "black"
+            cell.className = "cell alive"
         } else {
             cell.setAttribute("value", "dead")
-            cell.style.backgroundColor = "white"
+            cell.className = "cell dead"
         }
     })
 }
