111ed3c9dedffc4efc894263c990098a021603b1
ADragonDood <53982960+ADragonDood@users.noreply.github.com>
2021-12-01 12:16:38 +0000

    Add files via upload

--- js/index.js
+++ js/index.js
@@ -4,21 +4,30 @@ var clockSpeed;
 var dragList = []
 var grid = document.getElementById("grid")
 var programStarted = false;
+var preset;
+var originCell;
+var boundaryCells = []
+var settingOrigin = false;
+var settingBoundary = false;
+var presetName;
 
 let rngText = document.getElementById('rng-text')
 let rngInput = document.getElementById('clock-speed-rng')
 rngText.innerText = rngInput.value
 clockSpeed = parseInt(rngInput.value)
 
+
 rngInput.addEventListener("input", () => {
     rngText.innerText = rngInput.value
+
 })
 
 rngInput.addEventListener("change", () => {
     clockSpeed = parseInt(rngInput.value)
+    clearInterval(repeat)
     if (programStarted == true) {
         clearInterval(repeat)
-        repeat = setInterval(checkCell, clockSpeed)   
+        repeat = setInterval(checkCell, clockSpeed)
     }
 })
 
@@ -41,7 +50,10 @@ function createGrid() {
         grid.innerHTML = ""
     }
 
-    for (let i = 0; i < (grid.clientHeight/10)-1; i++) {
+    let gridWidth = 100
+    let gridHeight = 100
+
+    for (let i = 0; i < (gridHeight)+1; i++) {
         let rowObj = document.createElement("div");
         rowObj.className = "row";
         rowObj.id = "row"+i;
@@ -50,13 +62,13 @@ function createGrid() {
 
         let row = document.getElementById("row"+i);
 
-        for (let j = 0; j < (grid.clientWidth/10); j++) {
+        for (let j = 0; j < (gridWidth)+1; j++) {
 
             let cellObj = document.createElement("div");
             cellObj.className = "cell dead";
             cellObj.id = "cell-"+j+"-row-"+i;
             cellObj.setAttribute("value", "dead");
-            cellObj.setAttribute("onclick", "setState(this);");
+            cellObj.setAttribute("onclick", "setState(this); placePreset(this); setOriginCell(this); setBoundaryCell(this);");
             cellObj.setAttribute("ondragover", "addToDragList(this);")
             cellObj.setAttribute("ondragstart", "clearList();")
             cellObj.setAttribute("ondragend", "setDraggedCellsState();")
@@ -64,6 +76,44 @@ function createGrid() {
 
             row.appendChild(cellObj)
         }
+    }
+}
+
+function showLineOfSymmetry() {
+    let verticalSym = document.getElementById('vertical-sym').checked
+    let horizontalSym = document.getElementById('horizontal-sym').checked
+    let cellsPerRow = document.getElementsByClassName("row")[0].childElementCount
+    let rowCount = document.getElementsByClassName("row").length;
+
+    let middleOfPage = Math.round(rowCount/2)-1
+    let middleRow = document.getElementById('row' + middleOfPage)
+
+    let middleCells = []
+
+    if (verticalSym == true) {
+        middleRow.childNodes.forEach(node => {
+            node.style.backgroundColor = 'lightgray'
+        })
+    } else {
+        middleRow.childNodes.forEach(node => {
+            node.style.backgroundColor = ''
+        })
+    }
+
+    if (horizontalSym == true) {
+        for (let i = 0; i < rowCount; i++) {
+            middleCells.push(document.getElementById('cell-'+ (Math.round(cellsPerRow/2)-1) + "-row-"+i))
+        }
+        middleCells.forEach(cell => {
+            cell.style.backgroundColor = 'lightgray'
+        })
+    } else {
+        for (let i = 0; i < rowCount; i++) {
+            middleCells.push(document.getElementById('cell-'+ (Math.round(cellsPerRow/2)-1) + "-row-"+i))
+        }
+        middleCells.forEach(cell => {
+            cell.style.backgroundColor = ''
+        })
     }
 }
 
@@ -78,20 +128,59 @@ function clearList() {
 }
 
 function addToDragList(cell) {
+    let verticalSym = document.getElementById("vertical-sym").checked
+    let horizontalSym = document.getElementById("horizontal-sym").checked
+    let cellsPerRow = document.getElementsByClassName("row")[0].childElementCount
+    let rowCount = document.getElementsByClassName("row").length;
+    let cellIdSplit = cell.id.split("-")
+
     if (!dragList.includes(cell)) {
+        cell.style.backgroundColor = 'lightblue'
         dragList.push(cell)
+
+        if (verticalSym == true) {
+            let mirroredCell = document.getElementById("cell-" + (cellsPerRow-(parseInt(cellIdSplit[1])+1))+"-row-"+cellIdSplit[3])
+            mirroredCell.style.backgroundColor = 'lightblue'
+        }
+
+        if (horizontalSym == true) {
+            let mirroredCell = document.getElementById("cell-"+(parseInt(cellIdSplit[1]))+"-row-"+(rowCount-parseInt(cellIdSplit[3])-1))
+            mirroredCell.style.backgroundColor = 'lightblue'
+        }
+
+        if (horizontalSym == true && verticalSym == true) {
+            let mirroredCell = document.getElementById("cell-" + (cellsPerRow-(parseInt(cellIdSplit[1])+1))+"-row-"+(rowCount-parseInt(cellIdSplit[3])-1))
+            mirroredCell.style.backgroundColor = 'lightblue'
+        }
     }
 }
 
-function setState(cell) {
+function placePreset(cell) {
+    if (typeof preset == "undefined") return
+    let cellIdSplit = cell.id.split("-")
 
+    preset.forEach(pos => {
+        let newCell = document.getElementById("cell-"+(parseInt(cellIdSplit[1])+pos[0])+"-row-"+(parseInt(cellIdSplit[3])+pos[1]))
+        if (newCell == null) {
+            return
+        }
+        setState(newCell)
+    })
+
+}
+
+function setState(cell) {
+    if (settingOrigin == true || settingBoundary == true) return
     let verticalSym = document.getElementById("vertical-sym").checked
     let horizontalSym = document.getElementById("horizontal-sym").checked
+    let cellsPerRow = document.getElementsByClassName("row")[0].childElementCount
+    let rowCount = document.getElementsByClassName("row").length;
+    let cellIdSplit = cell.id.split("-")
+    cell.style.backgroundColor = ''
 
-    if (verticalSym == true) {
-        let cellsPerRow = document.getElementsByClassName("row")[0].childElementCount
-        let cellIdSplit = cell.id.split("-")
+    if (horizontalSym == true) {
         let mirroredCell = document.getElementById("cell-" + (cellsPerRow-(parseInt(cellIdSplit[1])+1))+"-row-"+cellIdSplit[3])
+        mirroredCell.style.backgroundColor = ''
 
         let value = mirroredCell.getAttribute("value")
         if (value == "dead") {
@@ -103,10 +192,9 @@ function setState(cell) {
         }
     }
 
-    if (horizontalSym == true) {
-        let rowCount = document.getElementsByClassName("row").length;
-        let cellIdSplit = cell.id.split("-")
+    if (verticalSym == true) {
         let mirroredCell = document.getElementById("cell-"+(parseInt(cellIdSplit[1]))+"-row-"+(rowCount-parseInt(cellIdSplit[3])-1))
+        mirroredCell.style.backgroundColor = ''
 
         let value = mirroredCell.getAttribute("value")
         if (value == "dead") {
@@ -120,10 +208,8 @@ function setState(cell) {
     }
 
     if (horizontalSym == true && verticalSym == true) {
-        let rowCount = document.getElementsByClassName("row").length;
-        let cellsPerRow = document.getElementsByClassName("row")[0].childElementCount
-        let cellIdSplit = cell.id.split("-")
         let mirroredCell = document.getElementById("cell-" + (cellsPerRow-(parseInt(cellIdSplit[1])+1))+"-row-"+(rowCount-parseInt(cellIdSplit[3])-1))
+        mirroredCell.style.backgroundColor = ''
 
         let value = mirroredCell.getAttribute("value")
         if (value == "dead") {
@@ -153,9 +239,8 @@ function Clear() {
         cell.className = "cell dead"
     })
 
-    programStarted = false
-
     clearInterval(repeat)
+    programStarted = false
 }
 
 function loadCachedPositions(cachedPositions) {
@@ -166,11 +251,12 @@ function loadCachedPositions(cachedPositions) {
         cellPos.setAttribute("value", "alive")
         cellPos.className = "cell alive"
     })
+    programStarted = false
 }
 
 function Pause() {
-    programStarted = false
     clearInterval(repeat)
+    programStarted = false
 }
 
 function Start() {
@@ -183,8 +269,8 @@ function Start() {
         }
     })
 
-    programStarted = true
     repeat = setInterval(checkCell, clockSpeed)
+    programStarted = true
 }
 
 function checkCell() {
@@ -264,4 +350,185 @@ function updateCells(updateArr) {
     })
 }
 
+function getPreset(presetElement) {
+    if (typeof preset != "undefined") return preset = undefined
+    let presetName = presetElement.getAttribute("value")
+    preset = data[0][presetName]
+}
+
+function setOriginCell(cell) {
+    if (settingOrigin == false) return
+    originCell = cell
+    originCell.style.backgroundColor = "pink"
+    settingOrigin = false
+    document.getElementById('setOrigin').style.display = 'none'
+    document.getElementById('setBoundary').style.display = ''
+}
+
+function setBoundaryCell(cell) {
+    if (settingBoundary == false) return
+    if (boundaryCells.length < 2) {
+        cell.style.backgroundColor = "lightblue"
+        boundaryCells.push(cell)
+    }
+    if (boundaryCells.length == 2) {
+        settingBoundary = false
+        document.getElementById('setBoundary').style.display = 'none'
+        finalisePreset()
+    }
+}
+
+function finalisePreset() {
+    let boundaryCell1 = boundaryCells[0]
+    let boundaryCell2 = boundaryCells[1]
+
+    let boundaryCell1Id = boundaryCell1.id.split("-")
+    let boundaryCell2Id = boundaryCell2.id.split("-")
+
+    let boundaryWidth;
+    let boundaryHeight;
+
+    if (parseInt(boundaryCell1Id[1]) > parseInt(boundaryCell2Id[1])) {
+        boundaryWidth = parseInt(boundaryCell1Id[1]) - parseInt(boundaryCell2Id[1])
+    } else if (parseInt(boundaryCell1Id[1]) < parseInt(boundaryCell2Id[1])){
+        boundaryWidth = parseInt(boundaryCell2Id[1]) - parseInt(boundaryCell1Id[1])
+    } else {
+        boundaryWidth = 0
+    }
+
+    if (parseInt(boundaryCell1Id[3]) > parseInt(boundaryCell2Id[3])) {
+        boundaryHeight = parseInt(boundaryCell1Id[3]) - parseInt(boundaryCell2Id[3])
+    } else if (parseInt(boundaryCell1Id[3]) < parseInt(boundaryCell2Id[3])){
+        boundaryHeight = parseInt(boundaryCell2Id[3]) - parseInt(boundaryCell1Id[3])
+    } else {
+        boundaryHeight = 0
+    }
+
+    let aliveCells = []
+
+    for (let i=0; i < boundaryHeight+1; i++) {
+        for(let j=0; j < boundaryWidth+1; j++) {
+            let currentCell = document.getElementById("cell-" + (parseInt(boundaryCell1Id[1]) + j) + "-row-" + (parseInt(boundaryCell1Id[3]) + i))
+            if (currentCell.getAttribute("value") == "alive") {
+                aliveCells.push(currentCell)
+            }
+        }
+    }
+
+    let originDifs = []
+
+    if (aliveCells.includes(originCell)) {
+        aliveCells.splice(aliveCells.indexOf(originCell,1))
+    } else {
+        aliveCells.push(originCell)
+    }
+
+    aliveCells.forEach(cell => {
+        let cellId = cell.id.split("-")
+        let originCellId = originCell.id.split("-")
+
+        let xDif = cellId[1] - originCellId[1]
+        let yDif = cellId[3] - originCellId[3]
+
+        originDifs.push([xDif, yDif])
+    })
+
+    let objString = {[presetName] : originDifs}
+
+    Object.assign(data[0], objString)
+
+    storePreset(objString)
+    createPresetHTMLElement()
+
+    boundaryCells.forEach(cell => {
+        cell.style.backgroundColor = ''
+    })
+    boundaryCells = []
+
+    originCell.style.backgroundColor = ''
+    originCell = undefined
+
+    document.getElementById('cancelPreset').style.display = 'none'
+
+}
+
+function getPresetsFromLocalStorage() {
+    let keys = Object.keys(localStorage)
+
+    keys.forEach(key => {
+        let presetObj = JSON.parse(localStorage.getItem(key))
+
+        presetName = key
+        
+        Object.assign(data[0], presetObj)
+
+        createPresetHTMLElement()
+    })
+
+    presetName = undefined
+}
+
+function storePreset(presetObj) {
+    localStorage.setItem(presetName, JSON.stringify(presetObj))
+}
+
+function createPresetHTMLElement() {
+    let presetDiv = document.createElement('div');
+    presetDiv.className = "preset";
+    presetDiv.setAttribute("onclick", "getPreset(this);");
+    presetDiv.setAttribute("value", presetName);
+
+    document.getElementById('presets-list').appendChild(presetDiv)
+
+    let presetHeader = document.createElement('div');
+    presetHeader.className = "preset-header";
+    presetHeader.innerHTML = '<h4>'+presetName+'</h4><img class="delete-icon" src="./imgs/delete-icon.png" onclick="deletePreset(this);">';
+
+    presetDiv.appendChild(presetHeader)
+}
+
+function setPresetBoundary() {
+    settingBoundary = true
+}
+
+function setPresetOrigin() {
+    settingOrigin = true
+}
+
+function cancelPreset() {
+    settingBoundary = false
+    settingOrigin = false
+    if (boundaryCells.length > 0) {
+        boundaryCells.forEach(cell => {
+            cell.style.backgroundColor = ''
+        })
+        boundaryCells = []
+    }
+
+    if (typeof originCell != "undefined") {
+        originCell.style.backgroundColor = ''
+        originCell = undefined
+    }
+
+    presetName = undefined
+    document.getElementById('setOrigin').style.display = 'none'
+    document.getElementById('setBoundary').style.display = 'none'
+    document.getElementById('cancelPreset').style.display = 'none'
+}
+
+function createPreset() {
+    presetName = prompt("Please enter the name of your preset")
+    if (presetName == '') return presetName = undefined
+    document.getElementById('setOrigin').style.display = ''
+    document.getElementById('cancelPreset').style.display = ''
+}
+
+function deletePreset(delete_icon) {
+    let presetDiv = delete_icon.parentElement.parentElement
+    
+    localStorage.removeItem(presetDiv.getAttribute("value"))
+    presetDiv.remove()
+}
+
 createGrid()
+getPresetsFromLocalStorage()
